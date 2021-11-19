ee2bc5f270fd1ab445e2576a7d54f177a711a89d
Preethi <preethi@Administrators-MacBook-Pro.local>
2021-11-19 16:44:32 +0530

    Added portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  resources :portfolios
   resources :blogs
   devise_for :users
   get 'something/about'
   get 'admin_dashboard', to: "pages#admin_dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
+
   get 'pages/about'
   get 'pages/cotact'
-
+  root to: 'pages#home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_19_074530) do
+ActiveRecord::Schema.define(version: 2021_11_19_110936) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "string"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
