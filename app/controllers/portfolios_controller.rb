ffe521426ebc81c33f32f6e7d8ddf4d88dfec6ba
Preethi <preethi@Administrators-MacBook-Pro.local>
2021-11-19 23:08:36 +0530

    Adding create new feature for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,26 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
   def index
     @portfolio_items = Portfolio.all
   end
+
   def new
+    @portfolio = Portfolio.new
   end
 
   def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully created." }
+        format.json { render :show, status: :created, location: @portfolio }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -13,5 +28,16 @@ class PortfoliosController < ApplicationController
 
   def update
 
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_portfolio
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
   end
 end
