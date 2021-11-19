91b37c63d5ed36e8ab4ce5dd24436305a73a1581
Preethi <preethi@Administrators-MacBook-Pro.local>
2021-11-19 22:30:12 +0530

    Added index action to portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,17 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
 end
