cdabb9458381631ac97c097f3109200023f3f0ba
Preethi <preethi@Administrators-MacBook-Pro.local>
2021-11-19 22:09:35 +0530

    Adding seeds file to generate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Creating blogs
+10.times do |i|
+  Blog.create!(
+    title: "Title #{i}",
+    body: "Body content of blog #{i}"
+  )
+end
+ puts "Created 10 blogs "
+#Skills
+5.times do |j|
+  Skill.create!(
+    title: "Skill #{j}",
+    percent_utilized: j*5
+  )
+end
+
+puts "Created 5 Skills"
+
+#Portfolio
+9.times do |p|
+Portfolio.create!(
+  title: "Portfolio title #{p}",
+  subtitle: "#{p} - subtitle",
+  body: "sdfsdfjhfjkshfierusdfjdsfjsd",
+  main_image:"http://placehold.it/600x400",
+  thumb_image:"http://placehold.it/250x200"
+)
+end
+
+puts "created 9 portfolios"
+
