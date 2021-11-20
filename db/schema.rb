f7db5565bcb0260230d7459a3b1d713d2d1b96b4
Preethi <preethi@Administrators-MacBook-Pro.local>
2021-11-21 00:13:33 +0530

    final commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_20_112106) do
+ActiveRecord::Schema.define(version: 2021_11_20_114513) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_11_20_112106) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
